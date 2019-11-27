57a5d216e73e1438b4f5764c63c3507e2550dd25
chenchen <ghjyjjj@gmail.com>
2019-11-28 02:13:19 +0800

    update

--- lineBot/app.js
+++ lineBot/app.js
@@ -43,12 +43,10 @@ bot.on('message',function(event){
 
 });
 
-
 const app = express();
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
 
-// //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8001, function() {
   var port = server.address().port;
   console.log("App now running on port", port);
